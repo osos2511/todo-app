4f3c0be93c3533d6f0fddac48474e6c43a761ce9
Osama mohamed <om329211@gmail.com>
2024-10-16 01:10:52 +0300

    +change ui

---
+++ lib/core/reusable_components/task_item.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TaskItem extends StatelessWidget {
+  const TaskItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: Row(
+        children: [
+          VerticalDivider(
+          ),
+        ],
+      ),
+    );
+  }
+}
